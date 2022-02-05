39cc505c2735c4fa4f93008e403e38a0f11fb405
MrMariu5 <marius.sadreika@gmail.com>
2022-02-05 19:39:29 +0200

    Completed

--- ReactProjects/react-projects-master/01-birthday-reminder/setup/src/App.js
+++ ReactProjects/react-projects-master/01-birthday-reminder/setup/src/App.js
@@ -2,7 +2,28 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people, setPeople] = useState(data);
+
+  return (
+  <main> 
+    <section className='container'>
+      <h3>{people.length} birthdays today</h3>
+      <List people = {people}/>
+      {
+        people.length > 0 &&
+        <button onClick={() => setPeople([])}>
+          Clear all
+        </button>
+      }
+      {
+        people.length == 0 &&
+        <button onClick={() => setPeople(data)}>
+          Refill list
+        </button>
+      }
+    </section>
+  </main>
+  );
 }
 
 export default App;
